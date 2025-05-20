c788262c497ce2b42433d6d43a10880f600e9216
rasyikka <rasyikassi@gmail.com>
2025-05-20 07:55:45 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("😽😽‼️‼️ I LOVE LANAAA")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "LANA MY WIFE GUYSSSS YOLOS"
 )
