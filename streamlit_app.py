fbc913b3f42ad134e78df3333dfd8836cfd0fbe3
rasyikka <rasyikassi@gmail.com>
2025-05-27 07:56:58 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,14 @@
 import streamlit as st
+
+st.title("😽😽‼️‼️ I LOVE LANAAA")
+st.write(
+    "LANA MY WIFE GUYSSSS YOLOS"
+)
+st.image("IMG_1027.png")
+
+#blenlebleblublublubleble
+
+import streamlit as st
 st.title("Aplikasi Sederhana")
 st.header("Aplikasi Mengecek Nilai Genap/Ganjil")
 angka = st.number_input("Tulis Sebuah Angka:", value=0, step=1)
